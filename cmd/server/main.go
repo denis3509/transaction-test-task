539c90d5c0367a7f1dab94f5e4bac37426114da3
denis <denis3509@gmail.com>
2023-09-23 07:58:16 +0300

    main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,54 +2,56 @@ package main
 
 import (
 	"fmt"
-	"transaction/pkg/log"
 	"transaction/internal/config"
+	"transaction/internal/transaction"
+	"transaction/pkg/db"
+	"transaction/pkg/log"
  
-	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
 	"syscall"
 
-	dbx "github.com/go-ozzo/ozzo-dbx"
+	"github.com/gin-gonic/gin"
+
 	_ "github.com/lib/pq"
 )
 
-func gracefullShutdown(log log.Logger){
- 	log.Info("gracefully shutdown")
+func gracefullShutdown(log log.Logger) {
+	log.Info("gracefully shutdown")
 }
 
-func main(){
+func main() {
 	cfg := config.GetConfig()
 
 	log := log.NewLogger(os.Stdout)
 
-	db, err := dbx.MustOpen("postgres", cfg.DB.DSN())
+	db, err := db.MustOpen("postgres", cfg.DB.DSN())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
- 
- 
-	go func () {
-		err  = http.ListenAndServe(":" + strconv.Itoa(cfg.Port) , nil)
-		if err != nil {
-			log.Fatal("Can't start server: ", err)
-		} 
+
+	router := gin.Default()
+	transaction.CreateRoutes(router, &db)
+	go func() {
+		connStr := fmt.Sprint("localhost:", strconv.Itoa(cfg.Port))
+		router.Run(connStr)
+
 	}()
-	log.Infof("server listening on localhost:%d",cfg.Port)
-	
-	// signal 
-    sigs := make(chan os.Signal, 1) 
-    signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM) 
-    done := make(chan bool, 1) 
-    go func() {
-        sig := <-sigs 
-        fmt.Printf("Received signal: %s \n",sig)
+	log.Infof("server listening on localhost:%d", cfg.Port)
+
+	// signal
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	done := make(chan bool, 1)
+	go func() {
+		sig := <-sigs
+		fmt.Printf("Received signal: %s \n", sig)
 		gracefullShutdown(log)
-        done <- true
-    }() 
-    <-done
-    fmt.Println("exiting")
- 
+		done <- true
+	}()
+	<-done
+	fmt.Println("exiting")
+
 }
